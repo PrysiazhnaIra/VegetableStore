fb6737fcff6ba9cf4c6ec198083a2cb67b10ec45
PrysiazhnaIra <ira.prisyazhna@gmail.com>
2024-03-14 09:28:33 +0200

    added section how it works

---
+++ script.js
@@ -0,0 +1,73 @@
+// змінні та типи даних
+// const age = 20;
+// let totalPrice = 100.42;
+// const userName = "Alice";
+// const isOpen = true;
+// let user = null;
+// let product;
+
+// totalPrice = 50.21; - can
+// age = 70; - can't
+
+// console.log(totalPrice);
+// console.log(typeof userName);
+
+// арифметичні операції
+
+// const x = 10;
+// const y = 7;
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y); //  залишок від ділення
+//console.log(x ** y); //  піднесення до степеня
+
+// перевизначення комбінованим оператором
+
+// let students = 100;
+// students = students + 50;
+// students += 50;
+// console.log(students);
+
+// пріорітет операторів
+
+// рядки
+// const firstName = "Alice";
+// const lastName = "Franko";
+
+// const fullName = firstName + " " + lastName; конкатинація
+
+// const sayHello = `Hello ${firstName}`;
+
+// console.log(sayHello); - шаблонні рядки
+
+// let message = "hello";
+
+// message = "la";
+
+// console.log(message[message.length - 1]);
+
+// Фраза за доп шаблонного рядка
+
+// const companyName = "Boston Dynamic";
+// const repairBots = 150;
+// const defenceBots = 50;
+
+// const message = `${companyName} has ${repairBots + defenceBots}`;
+
+// console.log(message);
+
+// перетворення типів даних
+// console.log(String(5));
+// console.log(typeof String(5));
+
+// console.log(String(true));
+// console.log(typeof String(true));
+
+// console.log(String(null));
+// console.log(typeof String(null));
+
+// console.log(String(undefined));
+// console.log(typeof String(undefined));
