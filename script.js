16526f5aceece51cbd6f7257226867e0201418f7
PrysiazhnaIra <ira.prisyazhna@gmail.com>
2024-07-23 13:27:32 +0300

    added condition to basket

--- script.js
+++ script.js
@@ -6,23 +6,31 @@ document.querySelectorAll(".vegetables-item").forEach((item) => {
 
   let quantity = 0;
 
-  decreaseBtn.addEventListener("click", () => {
+  decreaseBtn.onclick = () => {
     if (quantity > 0) {
       quantity--;
       quantityDisplay.textContent = quantity;
     }
-  });
+  };
 
-  increaseBtn.addEventListener("click", () => {
+  increaseBtn.onclick = () => {
     quantity++;
     quantityDisplay.textContent = quantity;
-  });
+  };
 
-  basket.addEventListener("click", () => {
-    iziToast.success({
-      title: "Success",
-      message: "The product has been added to your cart",
-      position: "topRight",
-    });
-  });
+  basket.onclick = () => {
+    if (quantity == 0) {
+      iziToast.info({
+        title: "Info",
+        message: "You should add a product!",
+        position: "topRight",
+      });
+    } else {
+      iziToast.success({
+        title: "Success",
+        message: "The product has been added to your cart",
+        position: "topRight",
+      });
+    }
+  };
 });
